2de7c869374b122ff85b520a37d065100ad9a84e
Aucons94 <148054502+Aucons94@users.noreply.github.com>
2023-12-13 22:25:06 +0100

    update

--- assets/js/player.js
+++ assets/js/player.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const audio = document.getElementById("audioPlayer");
   const progressBar = document.getElementById("progressBar");
   const playPauseBtn = document.getElementById("playPauseBtn");
+  const volumeInput = document.getElementById("myRange");
 
   playPauseBtn.addEventListener("click", function () {
     if (audio.paused) {
@@ -21,5 +22,9 @@ document.addEventListener("DOMContentLoaded", function () {
   audio.addEventListener("ended", function () {
     playPauseBtn.innerHTML = '<i class="bi bi-play-circle-fill"></i>';
     progressBar.style.width = "0%";
+  });
+
+  volumeInput.addEventListener("input", function () {
+    audio.volume = parseFloat(volumeInput.value);
   });
 });
