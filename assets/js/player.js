23938a6a9f71d2c82efc28bc59b35e777a774af5
Alf <cecerealfonso98@gmail.com>
2023-12-13 20:30:03 +0100

    progressbar

---
+++ assets/js/player.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const audio = document.getElementById("audioPlayer");
+  const progressBar = document.getElementById("progressBar");
+  const playPauseBtn = document.getElementById("playPauseBtn");
+
+  playPauseBtn.addEventListener("click", function () {
+    if (audio.paused) {
+      audio.play();
+      playPauseBtn.innerHTML = '<i class="bi bi-pause-circle-fill"></i>';
+    } else {
+      audio.pause();
+      playPauseBtn.innerHTML = '<i class="bi bi-play-circle-fill"></i>';
+    }
+  });
+
+  audio.addEventListener("timeupdate", function () {
+    const progress = (audio.currentTime / audio.duration) * 100;
+    progressBar.style.width = `${progress}%`;
+  });
+
+  audio.addEventListener("ended", function () {
+    playPauseBtn.innerHTML = '<i class="bi bi-play-circle-fill"></i>';
+    progressBar.style.width = "0%";
+  });
+});
