b3f699551ae380876be1918ed947797d42163e83
Alice Alessandrini <alice.alessandrini.12.12.1993@gmail.com>
2023-12-14 18:28:21 +0100

    Artist con Album

--- assets/js/artist.js
+++ assets/js/artist.js
@@ -49,78 +49,120 @@ function searchSongs(id) {
       }
     })
     .then((songs) => {
-      for (let i = 0; i < 10; i++) {
-        const row = document.createElement("div");
-        row.className = "row d-flex mb-3 g-0 song";
+      showFiveSongs(songs, 5);
 
-        const inDiv1 = document.createElement("div");
-        inDiv1.className = "col-1 d-flex justify-content-center align-items-center text-white-50";
-        inDiv1.innerHTML = i + 1;
-        const inDiv2 = document.createElement("div");
-        inDiv2.className = "col";
-        /* -------- */
-        const divImg = document.createElement("div");
-        divImg.className = "d-flex";
-
-        const inDivImg1 = document.createElement("div");
-        inDivImg1.className = "me-2 flex-shrink-0";
-        const img = document.createElement("img");
-        img.className = "w-100";
-        img.src = songs.data[i].album["cover_small"];
-
-        const inDivImg2 = document.createElement("div");
-        inDivImg2.className = "d-flex fs-6 m-0 flex-grow-1";
-
-        const title = document.createElement("p");
-        title.className = "mb-0";
-        title.innerText = songs.data[i].title;
-
-        inDivImg2.appendChild(title);
-        inDivImg1.appendChild(img);
-        divImg.appendChild(inDivImg1);
-        divImg.appendChild(inDivImg2);
-
-        inDiv2.appendChild(divImg);
-        /* -------- */
-
-        const inDiv3 = document.createElement("div");
-        inDiv3.className = "col-2 d-flex justify-content-center align-items-center text-white-50";
-        const p = document.createElement("p");
-        p.className = "mb-0";
-        const views = Math.round(Math.random() * 700000) + 3000000;
-        p.innerHTML = views;
-        inDiv3.appendChild(p);
-
-        const inDiv4 = document.createElement("div");
-        inDiv4.className = "col-2 d-flex justify-content-center align-items-center text-white-50";
-        const duration = document.createElement("p");
-        duration.className = "mb-0";
-        const resto =
-          songs.data[i].duration % 60 < 10 ? "0" + (songs.data[i].duration % 60) : songs.data[i].duration % 60;
-        duration.innerHTML = `${parseInt(songs.data[i].duration / 60)}:${resto}`;
-        inDiv4.appendChild(duration);
-
-        row.appendChild(inDiv1);
-        row.appendChild(inDiv2);
-        row.appendChild(inDiv3);
-        row.appendChild(inDiv4);
-
-        lista.appendChild(row);
-        row.addEventListener("click", function () {
-          const footerImg = document.getElementById("footerImg").querySelector("img");
-          footerImg.src = songs.data[i].album["cover_small"];
-          const footerSong = document.getElementById("footerSong").querySelectorAll("p");
-          footerSong[0].innerHTML = songs.data[i].title;
-          footerSong[1].innerHTML = songs.data[i].artist.name;
-          const audioContainer = document.getElementById("audioPlayer");
-          audioContainer.src = songs.data[i].preview;
-          const progressBar = document.getElementById("progressBar");
-          progressBar.style.width = 0;
-          const playPauseBtn = document.getElementById("playPauseBtn");
-          const audio = document.getElementById("audioPlayer");
-          audio.play();
-          playPauseBtn.innerHTML = '<i class="bi bi-pause-circle-fill"></i>';
-        });
-      }
+      const moreBtn = document.getElementById("showMore");
+      moreBtn.addEventListener("click", function () {
+        if (moreBtn.innerText == "VISUALIZZA ALTRO") {
+          showFiveSongs(songs, 10);
+          moreBtn.innerText = "MOSTRA MENO";
+        } else {
+          for (let i = lista.children.length - 1; i >= 5; i--) {
+            lista.children[i].remove();
+            moreBtn.innerText = "VISUALIZZA ALTRO";
+          }
+        }
+      });
+      showAlbums(songs.data);
     });
+}
+
+function showFiveSongs(songs, limit) {
+  let i = limit - 5;
+  for (i; i < limit; i++) {
+    const row = document.createElement("div");
+    row.className = "row d-flex mb-3 g-0 song";
+
+    const inDiv1 = document.createElement("div");
+    inDiv1.className = "col-1 d-flex justify-content-center align-items-center text-white-50";
+    inDiv1.innerHTML = i + 1;
+    const inDiv2 = document.createElement("div");
+    inDiv2.className = "col";
+    /* -------- */
+    const divImg = document.createElement("div");
+    divImg.className = "d-flex";
+
+    const inDivImg1 = document.createElement("div");
+    inDivImg1.className = "me-2 flex-shrink-0";
+    const img = document.createElement("img");
+    img.className = "w-100";
+    img.src = songs.data[i].album["cover_small"];
+
+    const inDivImg2 = document.createElement("div");
+    inDivImg2.className = "d-flex fs-6 m-0 flex-grow-1";
+
+    const title = document.createElement("p");
+    title.className = "mb-0";
+    title.innerText = songs.data[i].title;
+
+    inDivImg2.appendChild(title);
+    inDivImg1.appendChild(img);
+    divImg.appendChild(inDivImg1);
+    divImg.appendChild(inDivImg2);
+
+    inDiv2.appendChild(divImg);
+    /* -------- */
+
+    /*------*/
+
+    const inDiv3 = document.createElement("div");
+    inDiv3.className = "col-2 d-flex justify-content-center align-items-center text-white-50";
+    const p = document.createElement("p");
+    p.className = "mb-0";
+    const views = Math.round(Math.random() * 700000) + 3000000;
+    p.innerHTML = views;
+    inDiv3.appendChild(p);
+
+    const inDiv4 = document.createElement("div");
+    inDiv4.className = "col-2 d-flex justify-content-center align-items-center text-white-50";
+    const duration = document.createElement("p");
+    duration.className = "mb-0";
+    const resto =
+      songs.data[i].duration % 60 < 10 ? "0" + (songs.data[i].duration % 60) : songs.data[i].duration % 60;
+    duration.innerHTML = `${parseInt(songs.data[i].duration / 60)}:${resto}`;
+    inDiv4.appendChild(duration);
+
+    row.appendChild(inDiv1);
+    row.appendChild(inDiv2);
+    row.appendChild(inDiv3);
+    row.appendChild(inDiv4);
+
+    lista.appendChild(row);
+    row.addEventListener("click", function () {
+      const footerImg = document.getElementById("footerImg").querySelector("img");
+      footerImg.src = songs.data[i].album["cover_small"];
+      const footerSong = document.getElementById("footerSong").querySelectorAll("p");
+      footerSong[0].innerHTML = songs.data[i].title;
+      footerSong[1].innerHTML = songs.data[i].artist.name;
+      const audioContainer = document.getElementById("audioPlayer");
+      audioContainer.src = songs.data[i].preview;
+      const progressBar = document.getElementById("progressBar");
+      progressBar.style.width = 0;
+      const playPauseBtn = document.getElementById("playPauseBtn");
+      const audio = document.getElementById("audioPlayer");
+      audio.play();
+      playPauseBtn.innerHTML = '<i class="bi bi-pause-circle-fill"></i>';
+    });
+  }
+}
+let idAlbumArray = [];
+/*funzioni cards discografia*/
+function showAlbums(songs) {
+  console.log(songs);
+  songs.forEach((song) => {
+    if (!idAlbumArray.includes(song.album.id) && idAlbumArray.length < 5) {
+      idAlbumArray.push(song.album.id);
+      let albumCard = document.createElement("div");
+      albumCard.className = "col";
+      albumCard.innerHTML = `<div class="custom-card-bg p-2 rounded-1 text-nowrap overflow-hidden mb-3">
+     <img src="${song.album.cover_medium}" alt="cover" class="w-100 rounded-1 mb-4" />
+     <h6>${song.album.title}</h6>
+   </div>`;
+      let albumRow = document.getElementById("discList");
+      albumRow.appendChild(albumCard);
+      albumCard.addEventListener("click", function () {
+        window.location.assign("./album.html?albumId=" + song.album.id);
+      });
+    }
+  });
 }
