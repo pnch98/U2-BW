4100aa8431285291bf5bd57a17ab86225ebe5847
Alf <cecerealfonso98@gmail.com>
2023-12-14 13:04:02 +0100

    cestamoaprova

--- assets/js/album.js
+++ assets/js/album.js
@@ -1,5 +1,8 @@
-const url = "https://deezerdevs-deezer.p.rapidapi.com/album/";
 const albumId = new URLSearchParams(window.location.search).get("albumId");
+const url =
+  albumId == "liked"
+    ? "https://deezerdevs-deezer.p.rapidapi.com/track/"
+    : "https://deezerdevs-deezer.p.rapidapi.com/album/";
 const options = {
   method: "GET",
   headers: {
@@ -9,16 +12,19 @@ const options = {
 };
 
 window.addEventListener("DOMContentLoaded", () => {
-  fetch(url + albumId, options)
-    .then((resp) => {
-      if (resp.ok) {
-        return resp.json();
-      }
-    })
-    .then((obj) => {
-      showCover(obj);
-      showResults(obj.tracks.data);
-    });
+  if (albumId == "liked") {
+  } else {
+    fetch(url + albumId, options)
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.json();
+        }
+      })
+      .then((obj) => {
+        showCover(obj);
+        showResults(obj.tracks.data);
+      });
+  }
 });
 
 const songDiv = document.getElementById("song");
@@ -47,18 +53,53 @@ function showResults(album) {
     const views = Math.round(Math.random() * 700000) + 3000000;
     p.innerHTML = views;
     td3.appendChild(p);
+
     const td4 = document.createElement("td");
+    td4.style = "width: 60px";
+    td4.innerHTML = '<i class="bi bi-heart d-none"></i>';
+
+    if (likedSongs.includes(song.id)) {
+      td4.innerHTML = '<i class="bi bi-heart-fill d-none"></i>';
+    }
+
+    td4.addEventListener("click", () => {
+      if (td4.innerHTML == '<i class="bi bi-heart"></i>') {
+        td4.innerHTML = '<i class="bi bi-heart-fill"></i>';
+        likedSongs.push(song.id);
+        localStorage.setItem("likedSongs", likedSongs);
+        console.log(likedSongs);
+        console.log(localStorage.getItem("likedSongs"));
+      } else {
+        td4.innerHTML = '<i class="bi bi-heart"></i>';
+        const indexSong = likedSongs.indexOf(song.id);
+        likedSongs.splice(indexSong, 1);
+        localStorage.setItem("likedSongs", likedSongs);
+        console.log(likedSongs);
+        console.log(localStorage.getItem("likedSongs"));
+      }
+    });
+
+    const td5 = document.createElement("td");
     const duration = document.createElement("p");
     duration.className = "mb-0 ms-auto";
     const resto = song.duration % 60 < 10 ? "0" + (song.duration % 60) : song.duration % 60;
     duration.innerHTML = `${parseInt(song.duration / 60)}:${resto}`;
-    td4.appendChild(duration);
+    td5.appendChild(duration);
+
+    const td6 = document.createElement("td");
+    td6.style = "width: 60px";
+    td6.innerHTML = '<i class="bi bi-three-dots d-none"></i>';
+
     tr.appendChild(td1);
     tr.appendChild(td2);
     tr.appendChild(td3);
     tr.appendChild(td4);
+    tr.appendChild(td5);
+    tr.appendChild(td6);
+
     songDiv.appendChild(tr);
-    tr.addEventListener("click", function () {
+
+    td2.addEventListener("click", function () {
       const footerImg = document.getElementById("footerImg").querySelector("img");
       footerImg.src = song.album["cover_small"];
       const footerSong = document.getElementById("footerSong").querySelectorAll("p");
@@ -72,6 +113,17 @@ function showResults(album) {
       const audio = document.getElementById("audioPlayer");
       audio.play();
       playPauseBtn.innerHTML = '<i class="bi bi-pause-circle-fill"></i>';
+    });
+
+    tr.addEventListener("mouseover", () => {
+      tr.style = "cursor: pointer";
+      td4.firstChild.classList.remove("d-none");
+      td6.firstChild.classList.remove("d-none");
+    });
+
+    tr.addEventListener("mouseout", () => {
+      td4.firstChild.classList.add("d-none");
+      td6.firstChild.classList.add("d-none");
     });
     i += 1;
   });
--- assets/js/likes.js
+++ assets/js/likes.js
@@ -1,1 +1,12 @@
-const likedSongs = localStorage.getItem("likedSongs") ? localStorage.getItem("likedSongs") : [];
+let likedSongs = []; // dichiaro l'array vuoto
+
+if (localStorage.getItem("likedSongs")) {
+  // controlla se nel local storage è già presente un array che contiene canzoni a cui l'utente ha messo mi piace
+
+  //prendiamo l'array del localstorage che è in formato stringa, splittiamo la stringa alla virgola
+  const str = Array.from(localStorage.getItem("likedSongs").split(",")); //ritorna un array con gli id sotto forma di stringa
+
+  str.forEach((id) => {
+    likedSongs.push(parseInt(id)); // per ogni id (sotto forma di stringa) lo parsiamo ad intero e lo pushiamo nel nostro array di likedSongs
+  });
+}
